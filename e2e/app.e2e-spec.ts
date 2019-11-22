b231750bb556685f4b701c38fc4435afc4fae1c9
Ismail Sener <ismailsener@Ismails-MacBook-Pro.local>
2019-11-22 00:13:46 +0000

    update e2e

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { AppPage } from './app.po';
+import { protractor } from 'protractor/built/ptor';
 
 describe('angular-weather App', () => {
   let page: AppPage;
@@ -13,12 +14,6 @@ describe('angular-weather App', () => {
     page.fillFormAndSubmit('Londland');
 
     expect(page.getErrorMessage()).toBeTruthy();
-  });
-
-  it('when submitting form, user should be able to see Loading text indicator in table', () => {
-    page.fillFormAndSubmit('London');
-
-    expect(page.getLastAddedCityName()).toEqual('London');
   });
 
   it('when user submitting form with correct city name they should see related data in table', () => {
